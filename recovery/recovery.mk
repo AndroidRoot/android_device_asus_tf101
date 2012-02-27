62ed7a05be16a1c8e87898f4674f36f8ae626c24
BuzzBumbleBee <bigbeeshane@gmail.com>
2012-02-27 03:37:35 +0000

    fix build bug: cyclical dependency -- from koush

--- recovery/recovery.mk
+++ recovery/recovery.mk
@@ -1,5 +1,6 @@
 TF_BLOBIFIER := $(HOST_OUT_EXECUTABLES)/blobpack_tf
 
+
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 		$(recovery_ramdisk) \
 		$(TF_BLOBIFIER) \
@@ -10,12 +11,11 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 	@echo ----- Made recovery image -------- $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 
-$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) \
-		$(recovery_ramdisk) \
-		$(TF_BLOBIFIER) \
-		$(recovery_kernel)
-	@echo ----- Making recovery image ------
-	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) --output $@.orig
+
+$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(TF_BLOBIFIER)
+	$(call pretty,"Target boot image: $@")
+	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) --output $@.orig
 	$(TF_BLOBIFIER) $@ LNX $@.orig
-	@echo ----- Made boot image -------- $@
-	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
+
+
